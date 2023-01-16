dce28504f287ead74b45b286e3a21ef531688672
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-16 23:41:20 +0530

    Update FinalProject.java

--- FinalProject.java
+++ FinalProject.java
@@ -190,3 +190,64 @@ public class FinalProject {
     }
 
 }
+
+/*
+
+Output:
+
+---------
+|       |
+|       |
+|       |
+---------
+2 2
+---------
+|       |
+|   X   |
+|       |
+---------
+2 2
+This cell is occupied! Choose another one!
+two two
+You should enter numbers!
+1 4
+Coordinates should be from 1 to 3!
+1 1
+---------
+| O     |
+|   X   |
+|       |
+---------
+3 3
+---------
+| O     |
+|   X   |
+|     X |
+---------
+2 1
+---------
+| O     |
+| O X   |
+|     X |
+---------
+3 1
+---------
+| O     |
+| O X   |
+| X   X |
+---------
+2 3
+---------
+| O     |
+| O X O |
+| X   X |
+---------
+3 2
+---------
+| O     |
+| O X O |
+| X X X |
+---------
+X wins
+
+ */
